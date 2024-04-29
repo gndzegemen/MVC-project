8f483aeddc4d7eb916dbeadd8c7d9b6e30a4fc47
“Egemen <gndzegemen@gmail.com>
2024-04-30 01:23:18 +0300

    create: mvc template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace inavasyon_task.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
