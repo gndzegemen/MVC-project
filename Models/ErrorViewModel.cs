ef10432491f6ffaa746638e4fd9a6be6e58bba9b
“Egemen <gndzegemen@gmail.com>
2024-04-30 15:06:40 +0300

    fix: sln

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace inavasyon_task.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
